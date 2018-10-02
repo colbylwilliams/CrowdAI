d0ede4dbc978988ebd83b83f271a3a7d7e22abd2
Colby Williams <colbylwilliams@gmail.com>
2018-10-02 10:44:30 -0400

    initial iOS Project

---
+++ iOS/CrowdAI/CrowdAI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CrowdAI
+//
+//  Created by Colby L Williams on 10/2/18.
+//  Copyright © 2018 Colby L Williams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
